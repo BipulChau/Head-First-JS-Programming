8fb9016e51e9c0b55a415a533d4e6662c1884f3e
Bipul Chaudhary <bipulchau@gmail.com>
2021-09-07 14:15:07 -0400

    commented out testing code of parseGuess function

--- Battleship/battleship.js
+++ Battleship/battleship.js
@@ -92,8 +92,8 @@ function parseGuess(guess) {
 } return null; 
 }
 
-console.log(parseGuess("A0"))
+/*console.log(parseGuess("A0")) // checking parseGuess function
 console.log(parseGuess("B6"))
 console.log(parseGuess("G3"))
 console.log(parseGuess("H0"))
-console.log(parseGuess("A7"))
+console.log(parseGuess("A7"))*/
