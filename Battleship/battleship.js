df74c1721d6a012b6e8029ab57830218178cec3a
Bipul Chaudhary <bipulchau@gmail.com>
2021-09-07 14:13:05 -0400

    Created parseGuess function to convert Alphabet into number

--- Battleship/battleship.js
+++ Battleship/battleship.js
@@ -62,7 +62,7 @@ let model = {
   },
 };
 
-/*model.fire("53"); // testing model object
+/*model.fire("53"); // testing model object 
 model.fire("06");
 model.fire("16");
 model.fire("26");
@@ -72,3 +72,28 @@ model.fire("44");
 model.fire("12");
 model.fire("11");
 model.fire("10");*/
+
+function parseGuess(guess) {
+  let alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+  if (guess === null || guess.length !== 2) {
+    alert("Oops, please enter a letter and a number on the board.");
+  } else {
+    let firstChar = guess.charAt(0);
+    let row = alphabet.indexOf(firstChar);
+    let column = guess.charAt(1);
+
+    if (isNaN(row) || isNaN(column)) {
+      alert("Oops, that isn't on the board");
+    } else if (row < 0 || row >= model.boardSize || column < 0 || column >= model.boardSize){
+      alert("Oops, that's off the board!");
+    } else {
+      return row + column;
+    }
+} return null; 
+}
+
+console.log(parseGuess("A0"))
+console.log(parseGuess("B6"))
+console.log(parseGuess("G3"))
+console.log(parseGuess("H0"))
+console.log(parseGuess("A7"))
